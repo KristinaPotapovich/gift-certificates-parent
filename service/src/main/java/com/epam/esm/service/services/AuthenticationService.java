fcad057423f268550c90a3efe9c199b6a236bb99
KristinaPotapovich <ryzhik1191@gmail.com>
2021-02-13 18:02:32 +0300

    Module 4. Authentication & Spring Security
    correct jwtUser

--- service/src/main/java/com/epam/esm/service/services/AuthenticationService.java
+++ service/src/main/java/com/epam/esm/service/services/AuthenticationService.java
@@ -3,6 +3,15 @@ package com.epam.esm.service.services;
 import com.epam.esm.service.dto.RequestTokenDto;
 import com.epam.esm.service.dto.ResponseTokenDto;
 
+/**
+ * The interface Authentication service.
+ */
 public interface AuthenticationService {
+    /**
+     * Gets authentication result.
+     *
+     * @param requestTokenDto the request token dto
+     * @return the authentication result
+     */
     ResponseTokenDto getAuthenticationResult(RequestTokenDto requestTokenDto);
 }
